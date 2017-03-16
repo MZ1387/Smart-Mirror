c9ec54db191eeb8869e62e2811b2d4a5e622dfb9
Daniel <mrdanielkang@gmail.com>
2017-03-16 00:33:44 -0700

    Fixed bugs

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,38 +1,44 @@
 window.onload=start;
 var google = new firebase.auth.GoogleAuthProvider();
+google.addScope("https://www.googleapis.com/auth/plus.login");
 google.addScope("https://www.googleapis.com/auth/calendar.readonly");
 google.addScope("https://www.googleapis.com/auth/gmail.readonly");
 google.addScope("https://www.googleapis.com/auth/user.addresses.read");
 
-
 var gUser;
 function start() {
 	firebase.auth().onAuthStateChanged(function(user) {
-		console.log(user);
 	  if (user) {
+	  	$("#logCheck").html("Hello " + user.displayName);
+	  	console.log(user);
 		gUser = user;
 	  } else {
-	    loginGoogle();
+	  	loginGoogle();
 	  }
 	});
 }
 
 function loginGoogle() {
-	firebase.auth().signInWithPopup(google).then(function(result){
+	firebase.auth().signInWithRedirect(google);
+	firebase.auth().getRedirectResult().then(function(result) {
 		if(result.credential) {
 			var token = result.credential.accessToken;
+			$("#logCheck").html("logged in");
 		}
-		gUser = result.user;
+		var gUser = result.user;
+		console.log(gUser);
 	}).catch(function(error) {
 		var errorCode = error.code;
 		var errorMessage = error.message;
 		var email = error.email;
 		var credential = error.credential;
-	});
+		console.log(error);
+	})
 }
 
 function signOut() {
 	firebase.auth().signOut().then(function() {
+		location.reload();
 	}).catch(function(error) {
 	});
 }
