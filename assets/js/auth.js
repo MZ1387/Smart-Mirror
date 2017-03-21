53d901cc8c0d274aae4395cefa8b99280972713c
Rutul Patel <rutul-patel@live.com>
2017-03-21 01:11:45 -0700

    Add fetch info from gmail api and fill it in html

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,11 +1,12 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyC4sIDrmu-pRTkiQM7oIZSXaadzSm_q6Kk",
-    authDomain: "smart-mirror-a2978.firebaseapp.com",
+    apiKey: 'AIzaSyC4sIDrmu-pRTkiQM7oIZSXaadzSm_q6Kk',
+    authDomain: 'smart-mirror-a2978.firebaseapp.com',
     databaseURL: "https://smart-mirror-a2978.firebaseio.com",
     storageBucket: "smart-mirror-a2978.appspot.com",
     messagingSenderId: "993274322267"
 };
+var gUser, fireBaseAuth, database;
 
 // Client ID and API key from the Developer Console
 var CLIENT_ID = '993274322267-hbj9vaa6g3mian4nerttlk259qj28e1h.apps.googleusercontent.com';
@@ -15,21 +16,24 @@ var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/gmail/v1/res
 
 // Authorization scopes required by the API; multiple scopes can be
 // included, separated by spaces.
-var SCOPES = 'profile https://www.googleapis.com/auth/gmail.readonly ' +
+var SCOPES = '' +
+    ' https://mail.google.com/ ' +
+    ' https://www.googleapis.com/auth/gmail.readonly ' +
 	' https://www.googleapis.com/auth/plus.login ' +
+        ' https://www.googleapis.com/auth/gmail.modify ' +
 	' https://www.googleapis.com/auth/calendar.readonly ' +
-	' https://www.googleapis.com/auth/gmail.metadata ' +
+	// ' https://www.googleapis.com/auth/gmail.metadata ' +
 	' https://www.googleapis.com/auth/user.addresses.read ';
 
-
-var gUser, fireBaseAuth, database;
+firebase.initializeApp(config);
+fireBaseAuth = firebase.auth();
+database = firebase.database();
 
 /**
  *  On load, called to load the auth2 library and API client library.
  */
 function handleClientLoad() {
 	gapi.load('client:auth2', initClient);
-	console.log('handling client load');
 }
 
 /**
@@ -57,9 +61,6 @@ function initClient() {
 function updateSigninStatus(isSignedIn) {
 	console.log(isSignedIn);
     if (isSignedIn) {
-        firebase.initializeApp(config);
-        fireBaseAuth = firebase.auth();
-        database = firebase.database();
 
         fireBaseAuth.onAuthStateChanged(function(user) {
             if (user) {
@@ -69,9 +70,11 @@ function updateSigninStatus(isSignedIn) {
             }
         });
 
-        //makeAPICalls()
-        listLabels();
-        getIdList();
+
+        /**
+         * MAKE API CALLS HERE
+         */
+        getIdList('me', false, 'INBOX', 10);
     } else {
         var btn = $("<button>").text("Login");
         $("#logCheck").html(btn);
--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,113 +1,88 @@
 /**
- * Created by rutul on 3/18/17.
+ * Gets list of Id based on maxResults requested
+ * @param userId
+ * @param includeSpam
+ * @param labelIds
+ * @param maxResults
  */
-
-//
-// function getEmailsIds() {
-//     if(gUser) {
-//         console.log(gUser.email);
-//
-//
-//         // var q = $(this).attr("data-name");
-//         // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-//         //     q + "&api_key=dc6zaTOxFJmzC&limit=" + respLimit;
-//         // $("#resultsContainer").empty();
-//         // $.ajax({
-//         //     url: queryURL,
-//         //     method: "GET"
-//         // }).done(function(response) {
-//         //     var data = response.data;
-//         //     for (var i = 0; i < data.length; i++) {
-//         //         addGif(data[i].rating, data[i].images.fixed_height.url, data[i].images.fixed_height.height);
-//         //     }
-//         // });
-//         //
-//         // //
-//          var email = gUser.email;
-//          var emailID =  "15ae30fd56dded03";
-//         // var apikey = "DdG8xCrWLfQNGGwtZ1KeddL6Vem1";
-//         //
-//         // var queryURL = "https://www.googleapis.com/gmail/v1/users/" +
-//         //         email + "/messages/" + "rutulpatel.rtl%40gmail.com"
-//         //         + "?format=metadata&key=" + apikey;
-//         //
-//         // console.log(queryURL);
-//         //
-//         // $.ajax({
-//         //     url: queryURL,
-//         //     method: "GET"
-//         // }).done(function(response) {
-//         //     var data = response.data;
-//         //     console.log("RESPONSE", response);
-//         //     console.log("DATA", data);
-//         // });
-//     }
-// }
-
-
-console.log("MAIL.JS");
-/**
- * Print all Labels in the authorized user's inbox. If no labels
- * are found an appropriate message is printed.
- */
-function listLabels() {
-    gapi.client.gmail.users.labels.list({
-        'userId': 'me'
-    }).then(function(response) {
-        console.log(response);
-    });
-}
-
-
-function getIdList() {
-    console.log("CALLED - getIDList");
+function getIdList(userId, includeSpam, labelIds, maxResults) {
     gapi.client.gmail.users.messages.list({
-        'userId' : 'me',
-        includeSpamTrash: false,
-        labelIds : 'INBOX',
-        maxResults : 3
+        'userId' : userId,
+        includeSpamTrash: includeSpam,
+        labelIds : labelIds,
+        maxResults : maxResults
     }).then(function (response) {
-        //print output to console
-        console.log(response.result.messages);
-
-
         var messages = response.result.messages;
-        //appendPre('Messages:');
-
         if ( messages && messages.length > 0) {
             for (i = 0; i < messages.length; i++) {
                 var message = messages[i];
-                //appendPre(message.id);
-                console.log(message.id);
                 getEmailById(message.id);
             }
-        } else {
-            //appendPre('No Labels found.');
         }
     });
 }
 
+/**
+ * returns email body, needs to pass email's unique id
+ * @param emailId
+ */
 function getEmailById(emailId) {
-    console.log("CALLED - getEmailById");
     gapi.client.gmail.users.messages.get({
         'userId' : 'me',
         'id' : emailId,
-        'format': 'metadata'
+        'format' : 'full'
     }).then(function (response) {
         //print output to console.
-        console.log(response);
-
-
-//            var labels = response.result.messages;
-//            appendPre('Messages:');
-//
-//            if (labels && labels.length > 0) {
-//                for (i = 0; i < labels.length; i++) {
-//                    var label = labels[i];
-//                    appendPre(label.id)
-//                }
-//            } else {
-//                appendPre('No Labels found.');
-//            }
+        // console.log(response.result);
+        var headersArr = response.result.payload.headers;
+        var date = "", sender = "", sub = "";
+        for (var i = 0; i < headersArr.length; i++) {
+            switch (headersArr[i].name) {
+                case 'X-Received' :
+                    date = headersArr[i].value.match(/(mon|tue|wed|thu|fri|sat|sun).*(\d\d:\d\d:\d\d)/gi);
+                    break;
+                case 'Subject' :
+                    sub = headersArr[i].value;
+                    break;
+                case 'From' :
+                    sender = headersArr[i].value;
+                    break;
+                default:
+                    break;
+            }
+        }
+        if(date !== "" && sender !== "" && sub !== "" && response.result.snippet !== "") {
+            addEmailsToHTML(sender, sub, response.result.snippet, date);
+        }
     });
+}
+
+/**
+ * Adds email data into dynamically generated HTML
+ * @param sender
+ * @param sub
+ * @param desc
+ * @param date
+ */
+function addEmailsToHTML(sender, sub, desc, date) {
+    var emailMainDiv = $('<div>').attr({'class':'row email-container well'});
+    var iconDiv = $('<div>').attr({'class':'col-lg-2 col-md-2 col-sm-2 hidden-xs text-center'});
+    var icon = $('<i>').attr({'class':'icon ion-email hidden-xs email-icon'});
+    var emailContentDiv = $('<div>').attr({'class':'col-lg-10 col-md-10 col-sm-10 col-xs-12'}).html($('<div>').attr({'class':'row'}));
+    var nameDiv = $('<div>').attr({'class':'col-md-8 col-lg-8 col-sm-12 col-xs-12 email-sender-container'});
+    var nameText = $('<h1>').attr({'class':'email-sender'}).text(sender);
+    var dateDiv = $('<div>').attr({'class':'col-md-4 col-lg-4 col-sm-12 col-xs-12'});
+    var dateText = $('<h3>').attr({'class':'date'}).text(date);
+    var subDiv = $('<div>').attr({'class':'col-md-12 email-subject-container'});
+    var subText = $('<h3>').attr({'class':'email-sender'}).html($('<strong>').text(sub));
+    var descDiv = $('<div>').attr({'class':'col-md-12 email-description'});
+    var descText = $('<h3>').attr({'class':'email-description'}).text(desc);
+
+    $('.emails-container').append(emailMainDiv.append(iconDiv.html(icon))
+        .append(emailContentDiv
+            .append(nameDiv.html(nameText))
+            .append(dateDiv.html(dateText))
+            .append(subDiv.html(subText))
+            .append(descDiv.html(descText))
+        ));
 }
