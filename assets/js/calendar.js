ab8b1e3e254255cae45441bba0fddca4be4996ee
Daniel Kang <mrdanielkang@gmail.com>
2017-03-23 01:45:38 -0700

    updated calendar to sift through the events from the list retrieved to log any and all events and the start time. Next step will be to sort them into the id to be viewed.

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -11,5 +11,18 @@ function getCalendarList() {
 	}).then(function(response) {
 		var events = response.result.items;
 		console.log(response);
+//These next lines will be sifting through the events and adding them to the to do list
+		if(events.length>0) {
+			for(i=0; i < events.length; i++) {
+				var event = events[i];
+				var when = event.start.dateTime;
+				if(!when) {
+					when = event.start.date;
+				}
+				console.log(event.summary + ' (' + when + ')')
+			}
+		} else {
+			console.log("No upcoming events found");
+		}
 	});
 }
