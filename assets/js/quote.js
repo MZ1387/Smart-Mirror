3d6d1d258d601ec140893b64d64127854492b3f6
Mario Zamora <maza1387@gmail.com>
2017-03-25 04:35:21 -0700

    Update quote header, center text, new quote on page load

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -1,12 +1,18 @@
-$('.fa-quote-right').on('click', function(e) {
-  e.preventDefault();
+$(function() {
 
-  $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+  function getNewQuote() {
 
-      $("#newQuote").html(a[0].content + "<p>— " + a[0].title + "</p>")
-      console.log(a[0].content);
-      console.log(a[0].title);
+    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
 
-  });
+        $("#newQuote").html(a[0].content + "<p>— " + a[0].title + "</p>")
+        console.log(a[0].content);
+        console.log(a[0].title);
+        console.log("Quote works");
+
+    });
+
+  }
+
+getNewQuote();
 
 });
