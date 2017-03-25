1adfbafc759005457cdde0fbbcef856f7397480f
kptkid <mrdanielkang@gmail.com>
2017-03-25 06:49:57 -0700

    Fixed bug that was looping between just index and email

--- assets/js/loadPage.js
+++ assets/js/loadPage.js
@@ -5,9 +5,9 @@ function start() {
 	var timer = setInterval(function() {
 		if(dir === "index") {
 			window.location.href = "email.html";
-		} else if (dir === "email.html") {
+		} else if (dir === "email") {
 			window.location.href = "tickers.html";
-		} else if (dir === "tickers.html") {
+		} else if (dir === "tickers") {
 			window.location.href = "quote.html";
 		} else {
 			window.location.href = "index.html";
