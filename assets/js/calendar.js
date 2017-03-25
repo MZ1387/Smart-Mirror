4d1927ba6a67a7f4a973281c360b9a308fb7dafd
Daniel <mrdanielkang@gmail.com>
2017-03-25 01:20:38 -0700

    Calendar now updates HTML with day to day tasks on interval

---
+++ assets/js/calendar.js
@@ -0,0 +1,45 @@
+//Gets calendar list
+
+function getCalendarList() {
+	var nextTasks = true;
+	var timer = setInterval(function(){
+		if(nextTasks) {
+			nextTasks = false;
+		} else {
+			nextTasks = true;
+		}
+	}, 20000);
+
+	gapi.client.calendar.events.list({
+		'calendarId' :  'primary',
+		'timeMin' : (new Date()).toISOString(),
+		'showDeleted' : false,
+		'singleEvents' : true,
+		'maxResults' : 10,
+		'orderBy' : 'startTime'
+	}).then(function(response) {
+		var events = response.result.items;
+		console.log(response);
+//These next lines will be sifting through the events and adding them to the to do list
+		if(events.length>0) {
+			for(i=0; i < 8; i++) {
+				var event = events[i];
+				var when = event.start.dateTime;
+				if(!when) {
+					when = event.start.date;
+				}
+				console.log(event.summary + ' (' + when + ')');
+				if(nextTasks) {
+					$("#day-task-" + i).text(event.summary + ' (' + when + ')');
+
+				} else {
+					$("#day-task-" + (i-4)).text(event.summary + ' (' + when + ')');
+				}
+
+			}
+		} else {
+			console.log("No upcoming events found");
+			$("#day-task-1").text("No upcoming events found");
+		}
+	});
+}
