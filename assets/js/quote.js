bccabeeced594a07858fd385983bd9ff868152a8
Mario Zamora <maza1387@gmail.com>
2017-03-25 06:27:48 -0700

    Update quotes to https

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -2,7 +2,7 @@ $(function() {
 
   function getNewQuote() {
 
-    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+    $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
 
         $("#newQuote").html(a[0].content + "<p>— " + a[0].title + "</p>")
         console.log(a[0].content);
